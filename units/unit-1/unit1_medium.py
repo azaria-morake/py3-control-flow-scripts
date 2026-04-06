a51b7bccbb3728f40a3d292eff0396c863bb4ae6
azaria-morake <azariamorake@gmail.com>
2026-04-06 11:52:23 +0200

    Added unit1_medium.py

---
+++ units/unit-1/unit1_medium.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+current_temp = int(input("Enter current temperature: "))
+desired_temp = int(input("Enter desired temperature: "))
+system_status = input("Enter system status: ")
+
+def smart_thermo(current_temp, desired_temp, system_status):
+
+    if system_status == "maintenance":
+        print("System offline for repairs.")
+    elif system_status == "on":
+        if current_temp > (desired_temp + 2):
+            print("Cooling active.")
+        elif current_temp > (desired_temp - 2):
+            print("Heating active.")
+        else:
+            print("Temperature stable. Standby mode.")
+    else:
+        print("System is powered down.")
+
+smart_thermo(current_temp, desired_temp,
+   system_status)
