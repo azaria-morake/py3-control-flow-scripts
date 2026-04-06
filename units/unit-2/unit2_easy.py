11f67709c9f72cb599bbea4cf5cd3e781616ebcd
azaria-morake <azariamorake@gmail.com>
2026-04-06 16:26:04 +0200

    Added unit2_easy.py

---
+++ units/unit-2/unit2_easy.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+countdown = 10
+
+while countdown > 0:
+    if countdown == 5:
+        print("Halfway there!")
+    else:
+        print(countdown)
+
+    countdown-=1
+
+print("Blast Off!")
